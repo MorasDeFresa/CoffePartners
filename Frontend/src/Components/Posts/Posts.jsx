4c16c63c08e07e0a74c2cb56bc3a304587686634
MorasDeFresa <david@castromora.lat>
2024-05-21 20:23:29 -0500

    refactor(Posts): add index message

--- Frontend/src/Components/Posts/Posts.jsx
+++ Frontend/src/Components/Posts/Posts.jsx
@@ -1,22 +1,52 @@
 import { LazyLoadComponent } from "react-lazy-load-image-component";
 import SinglePost from "../SinglePost/SinglePost";
-import data from "../../data/PostBlog.json";
+// import data from "../../data/PostBlog.json";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Loading from "../Loading/Loading";
 import "./Posts.Module.css";
 
 function Posts() {
+  const [wait, setWait] = useState(true);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/api/Posts`)
+      .then(function (response) {
+        setWait(false);
+        setData(response.data?.reverse());
+      })
+      .catch(function (error) {
+        setWait(false);
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div className="cards">
-      {data.Posts?.map((post) => (
-        <LazyLoadComponent key={post.title}>
-          <SinglePost
-            title={post.title}
-            subtitle={post.subtitle}
-            imageUrl={post.imageUrl}
-            description={post.description}
-          ></SinglePost>
-        </LazyLoadComponent>
-      ))}
-    </div>
+    <>
+      {!wait ? (
+        <div className="cards">
+          {data.length < 1 && (
+            <p>
+              Parece que no hay actualizaciones, vuelve en otro momento
+              (´。＿。｀)
+            </p>
+          )}
+          {data?.map((post) => (
+            <LazyLoadComponent key={post.idPost}>
+              <SinglePost
+                title={post.titlePost}
+                subtitle={post.titlePost}
+                imageUrl={post.imgUrl}
+                description={post.descriptionPost}
+              ></SinglePost>
+            </LazyLoadComponent>
+          ))}
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 }
 
